c850fa18915153bcf6be394969d6e07b15428928
skywayoo <dorgonway@gmail.com>
2026-04-18 19:15:28 +0800

    Fix cron schedules (10min news/pubinfo), remove startup notification

--- instrumentation.ts
+++ instrumentation.ts
@@ -1,7 +1,3 @@
 export async function register() {
-  if (process.env.NEXT_RUNTIME === 'nodejs' && process.env.VERCEL_ENV) {
-    const { sendTelegram } = await import('@/lib/telegram');
-    const url = process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}` : '';
-    await sendTelegram(`🟢 台股助手已上線\n環境：${process.env.VERCEL_ENV}\n${url}`).catch(() => {});
-  }
+  // startup notification removed
 }
